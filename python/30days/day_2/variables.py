83dd40f4e379655cb759a619d78f3c310e26216f
eltetoncito <jarteagaaam23@gmail.com>
2026-05-26 07:28:11 -0600

    Add Day 2 exercises for variables and basic operations in Python

---
+++ python/30days/day_2/variables.py
@@ -0,0 +1,53 @@
+#Day 2: 30 days of python programming
+
+#Ejercicio 1
+first_name = input("Ingresa tu nombre: ")
+last_name = input("Ingresa tu apellido: ")
+full_name = "Maria Perez"
+country = input("Ingresa tu país: ")
+city = "Guanajuato"
+age = input("Ingresa tu edad: ")
+year = 2026
+is_married = True
+is_true = True
+is_light_on = False
+multiple = ["yo","tu","nosotros","ellos"]
+
+#----------------------
+#Ejercicio 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(multiple))
+
+
+print(len(first_name))
+print(len(last_name)-len(first_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radio = int(input("Ingresa el radio del circulo: "))
+area_of_circle = 3.14 * (radio ** 2)
+circum_of_circle = 2 * 3.14 * radio
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+print("El area del circulo es: ",area_of_circle)
+print("La circunferencia del circulo es: ",circum_of_circle)
